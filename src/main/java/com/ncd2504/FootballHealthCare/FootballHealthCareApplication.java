f03221ccf408bf580889f3e72e13636b6741b94a
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-09-06 14:57:54 +0700

    feat-SpringBoot Initlzer File Structure
    project folder structure using by Spring-Initializer
    
    CU-86b1wpjyt

---
+++ src/main/java/com/ncd2504/FootballHealthCare/FootballHealthCareApplication.java
@@ -0,0 +1,13 @@
+package com.ncd2504.FootballHealthCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballHealthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballHealthCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncd2504/FootballHealthCare/FootballHealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncd2504.FootballHealthCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballHealthCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
