d79a61d3d40284e5628d0023e33f0b7e1ea9d3e5
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-09-12 14:37:41 +0700

    feat-add model, repo, service for exercise
    add model, repo, service for exercise
    
    CU-86b246q2q

---
+++ src/main/java/com/ncd2504/FootballHealthCare/models/exercise.java
@@ -0,0 +1,53 @@
+package com.ncd2504.FootballHealthCare.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.ncd2504.FootballHealthCare.enums.MuscleEnum;
+
+@Document("exerciseInfor")
+public class exercise {
+    @Id
+    private String id;
+    private String name;
+    private String imageUrl;
+    private MuscleEnum targetMuscle1;
+    private MuscleEnum targetMuscle2;
+    private MuscleEnum targetMuscle3;
+
+    public exercise(String id,
+            String name,
+            String imageUrl,
+            MuscleEnum targetMuscle1, MuscleEnum targetMuscle2, MuscleEnum targetMuscle3) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.targetMuscle1 = targetMuscle1;
+        this.targetMuscle2 = targetMuscle2;
+        this.targetMuscle3 = targetMuscle3;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getImageUrl() {
+        return this.imageUrl;
+    }
+
+    public MuscleEnum getTargetMuscle1() {
+        return this.targetMuscle1;
+    }
+
+    public MuscleEnum getTargetMuscle2() {
+        return this.targetMuscle2;
+    }
+
+    public MuscleEnum getTargetMuscle3() {
+        return this.targetMuscle3;
+    }
+}
---
+++ src/main/java/com/ncd2504/FootballHealthCare/services/ExerciseService.java
@@ -0,0 +1,23 @@
+package com.ncd2504.FootballHealthCare.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ncd2504.FootballHealthCare.models.exercise;
+import com.ncd2504.FootballHealthCare.repositories.ExerciseRepository;
+
+@Service
+public class ExerciseService {
+    @Autowired
+    ExerciseRepository exerciseRepository;
+
+    public ExerciseService(ExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+
+    public Optional<exercise> getById(String id) {
+        return this.exerciseRepository.findById(id);
+    }
+}
